a3e80f72ede5a68caeaac65f7d136421b6df3e9f
Nataliia <dominok56@gmail.com>
2024-03-23 20:44:55 +0200

    added models

--- backend/links/models.py
+++ backend/links/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    full_link = models.TextField()
+    short_link = models.CharField(max_length=255)
+    title = models.CharField(max_length=127, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class LinkClick(models.Model):
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
+    clicked_at = models.DateTimeField(auto_now_add=True)
