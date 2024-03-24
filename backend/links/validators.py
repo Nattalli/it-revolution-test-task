0821a49b8d08ede100ebd42fc62f15178dcca4a7
Nataliia <dominok56@gmail.com>
2024-03-24 10:57:33 +0200

    removed unneeded link form the validator

--- backend/links/validators.py
+++ backend/links/validators.py
@@ -5,5 +5,5 @@ from .models import Link
 def validate_existing_link(value):
     if Link.objects.filter(full_link=value).exists():
         raise serializers.ValidationError(
-            f"This link is already exist in our database: {Link.objects.get(full_link=value).short_link}",
+            f"This link is already exist in our database:",
         )
